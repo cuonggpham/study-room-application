31a2964b879352948b754f58e38f030376cbf41e
cuonggpham <qcuonghls@gmail.com>
2024-08-03 01:59:30 +0700

    create CRUD

--- base/models.py
+++ base/models.py
@@ -9,6 +9,8 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
@@ -18,7 +20,8 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-
+    class Meta: 
+        ordering = ['-updated', '-created']
     def __str__(self):
         return self.name
     
